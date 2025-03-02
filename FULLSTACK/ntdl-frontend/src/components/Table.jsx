974dad61e7643befbac7cfc7837662cf0b8b429b
Bishes7 <specialbishes9@gmail.com>
2025-03-02 18:50:02 +1100

    functinality added for checkbox

--- FULLSTACK/ntdl-frontend/src/components/Table.jsx
+++ FULLSTACK/ntdl-frontend/src/components/Table.jsx
@@ -1,8 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Table = ({ taskList, switchTask, handleOnDelete }) => {
+  const [toDelete, setToDelete] = useState([]);
   const entryList = taskList.filter((item) => item.type === "entry");
   const badList = taskList.filter((item) => item.type === "bad");
+
+  const handleOnSelect = (e) => {
+    const { checked, value } = e.target;
+
+    if (checked) {
+      if (value === "allEntry") {
+        const allItems = entryList.map((item) => item._id);
+        setToDelete([...toDelete, ...allItems]);
+        return;
+      }
+      setToDelete([...toDelete, value]);
+    } else {
+      const deletedIds = toDelete.filter((_id) => _id !== value);
+      setToDelete(deletedIds);
+    }
+    console.log(checked, value);
+  };
+  console.log(toDelete);
 
   //   const totalBadHr =
   return (
@@ -10,20 +29,26 @@ const Table = ({ taskList, switchTask, handleOnDelete }) => {
       <div className="col-md text-center">
         <h3>Entry List</h3>
         <hr />
+        <input
+          type="checkbox"
+          value="allEntry"
+          id="all-entry"
+          onClick={handleOnSelect}
+        />
+        <label htmlFor="all-entry">Select All</label>
         <table className="table table-striped table-hover border shadow-lg">
           <tbody id="entryList">
             {entryList.map((item, i) => {
               return (
-                <tr key={item._id}>
+                <tr key={item?._id}>
                   <td>{i + 1}</td>
-
                   <td>
                     <div className="d-flex align-items-center ">
                       <input
+                        onClick={handleOnSelect}
                         className="form-check-input me-3 "
                         type="checkbox"
-                        value=""
-                        id=""
+                        value={item?._id}
                       />
                       {item.task}
                     </div>
@@ -53,6 +78,13 @@ const Table = ({ taskList, switchTask, handleOnDelete }) => {
       <div className="col text-center">
         <h3>Bad List</h3>
         <hr />
+        <input
+          type="checkbox"
+          value="allBad"
+          id="all-bad"
+          onClick={handleOnSelect}
+        />
+        <label htmlFor="allBad">Select All</label>
         <table className="table table-striped table-hover border shadow-lg">
           <tbody id="badlist">
             {badList.map((item, i) => {
@@ -62,9 +94,10 @@ const Table = ({ taskList, switchTask, handleOnDelete }) => {
                   <td>
                     <div className="d-flex align-items-center ">
                       <input
+                        onClick={handleOnSelect}
                         className="form-check-input me-3 "
                         type="checkbox"
-                        value=""
+                        value={item?._id}
                         id=""
                       />
                       {item.task}
