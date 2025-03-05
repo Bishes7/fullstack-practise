cc3ea86600cd144188431988a87b87f966af818c
Bishes7 <specialbishes9@gmail.com>
2025-03-06 00:00:39 +1100

    project completed

--- FULLSTACK/ntdl-frontend/axiosHelper/axiosHelper.js
+++ FULLSTACK/ntdl-frontend/axiosHelper/axiosHelper.js
@@ -1,6 +1,8 @@
 import axios from "axios";
 
-const apiEP = "http://localhost:8000/api/v1/tasks";
+const apiEP = import.meta.env.PROD
+  ? "/api/v1/tasks"
+  : "http://localhost:8000/api/v1/tasks";
 
 const apiProcessor = async ({ method, data }) => {
   try {
